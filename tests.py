ed51c7d9f72de5c1331a40741c0160320ecd8fd9
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-08-01 15:18:41 +0200

    Writing one more test

---
+++ tests.py
@@ -0,0 +1,55 @@
+import json
+import httpx
+
+
+def test_reserve_seats_from_empty_train():
+    """
+    Given no reservation at all
+    When we reserve 4 seats
+    We get 1A to 4A
+    """
+    train_id = "express_2000"
+
+    client = httpx.Client()
+    response = client.post("http://127.0.0.1:8081/reset", data={"train_id": train_id})
+    response.raise_for_status()
+
+    reservation = client.post(
+        "http://127.0.0.1:8083/reserve", data={"train_id": train_id, "seat_count": 4}
+    ).json()
+
+    last_booking_reference = client.get(
+        "http://127.0.0.1:8082/last_booking_reference"
+    ).text
+
+    assert reservation["train_id"] == "express_2000"
+    assert len(reservation["seats"]) == 4
+    assert reservation["seats"] == ["1A", "2A", "3A", "4A"]
+    assert reservation["booking_reference"] == last_booking_reference
+
+
+def test_reserve_four_additional_seats():
+    """
+    Given 4 seats already booked
+    When we reserve 4 seats
+    We get 5A to 8A
+    """
+    train_id = "express_2000"
+    client = httpx.Client()
+    response = client.post("http://127.0.0.1:8081/reset", data={"train_id": train_id})
+    response.raise_for_status()
+    reservation = client.post(
+        "http://127.0.0.1:8083/reserve", data={"train_id": train_id, "seat_count": 4}
+    ).json()
+
+    reservation = client.post(
+        "http://127.0.0.1:8083/reserve", data={"train_id": train_id, "seat_count": 4}
+    ).json()
+
+    last_booking_reference = client.get(
+        "http://127.0.0.1:8082/last_booking_reference"
+    ).text
+    assert reservation["train_id"] == "express_2000"
+    assert len(reservation["seats"]) == 4
+    assert reservation["seats"] == ["5A", "6A", "7A", "8A"]
+    assert reservation["booking_reference"] == last_booking_reference
