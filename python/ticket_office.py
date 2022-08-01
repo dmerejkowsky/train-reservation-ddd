005daaac466c7099b39750856adb9e5776091ecc
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-08-01 16:51:27 +0200

    Start using clean DDD objects in ticket_office

--- python/ticket_office.py
+++ python/ticket_office.py
@@ -1,19 +1,25 @@
-from httpx import Client
+import httpx
 import json
+
+from client import Client, TrainId
 
 
 class TicketOffice(object):
-    def __init__(self) -> None:
-        self.httpx_client = Client()
+    def __init__(self, *, client: Client) -> None:
+        self.client = client
+        self.httpx_client = httpx.Client()
 
-    def reserve(self, train_id: str, seat_count: str) -> str:
-        number_of_seats = int(seat_count)
-        train_data = self.httpx_client.get(
-            "http://localhost:8081/data_for_train/" + train_id
-        ).json()
-        available_seats = (
-            s for s in train_data["seats"].values() if s["booking_reference"] == ""
-        )
+    def reserve(self, *args: str) -> str:
+        # CherryPy stuff
+        assert len(args) == 2
+        train_id_str = args[0]
+        seat_count_str = args[1]
+
+        number_of_seats = int(seat_count_str)
+        train_id = TrainId(train_id_str)
+
+        manifest = self.client.get_manifest(train_id)
+        available_seats = (s for s in manifest.seats() if s.is_free)
         to_reserve = []
         for i in range(number_of_seats):
             to_reserve.append(next(available_seats))
@@ -21,7 +27,7 @@ class TicketOffice(object):
             "http://localhost:8082/booking_reference"
         ).text
 
-        seat_ids = [s["seat_number"] + s["coach"] for s in to_reserve]
+        seat_ids = [s.id for s in to_reserve]
         reservation = {
             "train_id": train_id,
             "booking_reference": booking_reference,
@@ -45,7 +51,8 @@ class TicketOffice(object):
 if __name__ == "__main__":
     """Deploy this class as a web service using CherryPy"""
     import cherrypy
+    from http_client import HttpClient
 
     TicketOffice.reserve.exposed = True  # type: ignore[attr-defined]
     cherrypy.config.update({"server.socket_port": 8083})
-    cherrypy.quickstart(TicketOffice())
+    cherrypy.quickstart(TicketOffice(client=HttpClient()))
