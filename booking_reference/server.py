e5c5cf4548c2bebb449ac7ef462cd177e3c0c94d
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-08-08 16:57:51 +0200

    Simplify ./booking_reference/

--- booking_reference/server.py
+++ booking_reference/server.py
@@ -33,36 +33,11 @@ class BookingReferenceService(object):
     last_booking_reference.exposed = True
 
 
-def main(args):
-    if args:
-        starting_point = int(args[0], 16) + 1
-    else:
-        starting_point = 123456789
-
+def main():
+    starting_point = 123456789
     cherrypy.config.update({"server.socket_port": 8082})
     cherrypy.quickstart(BookingReferenceService(starting_point))
 
 
 if __name__ == "__main__":
-    import sys
-
-    help_text = """
-Use this program to start a booking reference service:
-
-    python {0}
-
-The service will start on this url:
-
-    http://localhost:8082/booking_reference
-
-If you have to restart the service, you can continue counting from the
-previous reference by passing it on the command line:
-
-    python {0} 75bcd15
-    """.format(
-        sys.argv[0]
-    )
-    if "-help" in sys.argv or "--help" in sys.argv or "-h" in sys.argv:
-        print(help_text)
-    else:
-        main(sys.argv[1:])
+    main()
